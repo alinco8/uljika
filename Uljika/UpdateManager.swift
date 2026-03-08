a3304f73d36d5481e99d894fe9a43f4651c46cee
alinco8 <ali0207k@gmail.com>
2026-03-08 15:42:46 +0900

    fix: アップデートウィンドウを表示する際にアプリを最前面に
    closed #1

--- Uljika/UpdateManager.swift
+++ Uljika/UpdateManager.swift
@@ -1,11 +1,19 @@
 import Sparkle
+import AppKit
+
+final class UpdateUserDriverDelegate: NSObject, SPUStandardUserDriverDelegate {
+    func standardUserDriverWillShowModalAlert() {
+        NSApp.activate()
+    }
+}
 
 @Observable
 class UpdateManager {
     private let controller: SPUStandardUpdaterController
+    private let userDriverDelegate = UpdateUserDriverDelegate()
     
     init() {
-        controller = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+        controller = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: userDriverDelegate)
     }
     
     func checkForUpdates() {
